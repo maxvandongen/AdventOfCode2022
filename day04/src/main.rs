bce04c237b5e22b21c971dd73f587da5c74c677b
Max van Dongen <max.van.dongen@nl.abnamro.com>
2022-12-19 00:31:18 +0100

    Day04

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,75 @@
+use std::{collections::HashSet, fs};
+
+#[derive(Debug)]
+struct JobPair {
+    first: Job,
+    second: Job,
+}
+#[derive(Debug)]
+struct Job {
+    spots_to_do: HashSet<usize>,
+}
+
+impl JobPair {
+    fn has_unnecessary_job(self) -> bool {
+        self.first.fully_contains(&self.second) || self.second.fully_contains(&self.first)
+    }
+
+    fn has_any_overlap(self) -> bool {
+        self.first.has_any_overlap(&self.second)
+    }
+}
+
+impl From<&str> for JobPair {
+    fn from(value: &str) -> Self {
+        let mut parts = value.split(",");
+        JobPair {
+            first: parts.next().unwrap().into(),
+            second: parts.next().unwrap().into(),
+        }
+    }
+}
+
+impl Job {
+    fn fully_contains(&self, other: &Job) -> bool {
+        self.spots_to_do.is_superset(&other.spots_to_do)
+    }
+
+    fn has_any_overlap(&self, other: &Job) -> bool {
+        &self.spots_to_do.intersection(&other.spots_to_do).count() > &0
+    }
+}
+
+impl From<&str> for Job {
+    fn from(value: &str) -> Self {
+        let mut parts = value.split("-");
+        let left = parts.next().unwrap().parse::<usize>().unwrap();
+        let right = parts.next().unwrap().parse::<usize>().unwrap();
+        Job {
+            spots_to_do: HashSet::from_iter(left..=right),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "input.txt";
+    let pairs = fs::read_to_string(input)
+        .unwrap()
+        .split("\n")
+        .into_iter()
+        .map(|line| JobPair::from(line).has_unnecessary_job())
+        .filter(|b| *b)
+        .count();
+
+    println!("Found {} pairs with unnecessary jobs", pairs);
+
+    let overlaps = fs::read_to_string(input)
+        .unwrap()
+        .split("\n")
+        .into_iter()
+        .map(|line| JobPair::from(line).has_any_overlap())
+        .filter(|b| *b)
+        .count();
+
+    println!("Found {} pairs with overlapping jobs", overlaps);
 }
