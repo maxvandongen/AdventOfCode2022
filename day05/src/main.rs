6f607a9a78f226348fa79a488e98983771b4a61c
Max van Dongen <max.van.dongen@nl.abnamro.com>
2022-12-19 00:58:55 +0100

    Day 05 skeleton

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,46 @@
+use std::{collections::HashMap, fs};
+
+struct Crate {
+    text: String,
+}
+
+struct Stack {
+    stack: Vec<Crate>,
+}
+
+struct PlayingField {
+    stacks: HashMap<u32, Stack>,
+}
+
+struct Command {
+    from: u32,
+    to: u32,
+    amount: u32,
+}
+
+impl From<&str> for Command {
+    fn from(value: &str) -> Self {
+        todo!()
+    }
+}
+
+impl From<&str> for PlayingField {
+    fn from(value: &str) -> Self {
+        todo!()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_file = "example.txt";
+    let input_text = fs::read_to_string(input_file).unwrap();
+    // For now skipping the playing field
+    let commands: Vec<Command> = input_text
+        .split("\n\n")
+        .skip(1)
+        .next()
+        .unwrap()
+        .split("\n")
+        .into_iter()
+        .map(|line| line.into())
+        .collect();
 }
